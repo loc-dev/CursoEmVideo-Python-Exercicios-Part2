f9c36ffd7e041c7166d94caf54621305fb64b1e0
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-04 14:07:55 -0400

    Desafio 043 - Indice de Massa Corporal

---
+++ Exercicio_043/desafio_043.py
@@ -0,0 +1,27 @@
+#   Desafio 043 - Referente aula Fase12
+#   Desenvolva uma lógica que leia o peso e a altura
+#   de uma pessoa, calcule seu IMC e mostre seu status,
+#   de acordo com a tabela abaixo:
+
+# - Abaixo de 18.5 : Abaixo do Peso
+# - Entre 18.5 e 25 : Peso Ideal
+# - 25 até 30 : Sobrepeso
+# - 30 até 40 : Obesidade
+# - Acima de 40 : Obesidade Mórbida
+
+peso = float(input('Qual é o seu peso? (Kg) '))
+altura = float(input('Qual é a sua altura? (m) '))
+imc = peso / (altura ** 2)
+
+print('O IMC desse pessoa é de {:.1f}'.format(imc))
+
+if imc < 18.5:
+    print('Você está ABAIXO DO PESO normal !')
+elif 18.5 <= imc < 25:
+    print('PARABÉNS, você está na faixa de PESO NORMAL !')
+elif 25 <= imc < 30:
+    print('Você está em SOBREPESO !')
+elif 30 <= imc < 40:
+    print('Você está em OBESIDADE !')
+elif imc >= 40:
+    print('Você está em OBESIDADE MÓRBIDA, cuidado!')
