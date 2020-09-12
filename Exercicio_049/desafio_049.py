b67ec447ddc8e39d94c1e2eb57ef62392afd2547
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-12 11:06:42 -0400

    Desafio 049 - Tabuada v2.0

---
+++ Exercicio_049/desafio_049.py
@@ -0,0 +1,8 @@
+#   Desafio 049 - Referente aula Fase13
+#   Refaça o Desafio 009, mostrando a tabuada de um número
+#   que o usuário escolher, só que agora utilizando um laço for.
+
+n = int(input('Digite um número: '))
+
+for c in range(1, 11):
+    print('{} x {:2} = {}'.format(n, c, n * c))
