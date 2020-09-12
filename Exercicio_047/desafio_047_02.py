488b5b94375ee8ccd0a1d18fe4f46d78f2c21d32
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-12 10:39:13 -0400

    Desafio 047 - Contagem de pares - Versão 02 - Menos processamento

---
+++ Exercicio_047/desafio_047_02.py
@@ -0,0 +1,8 @@
+#   Desafio 047 - Referente aula Fase13
+#   Crie um programa que mostre na tela todos os números pares
+#   que estão no intervalo entre 1 e 50.
+
+#   Versão 02
+
+for n in range(2, 51, 2):
+    print(n, end=' ')
