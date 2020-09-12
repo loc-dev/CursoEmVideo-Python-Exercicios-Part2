368a2a96a77414d9f5ed2d3ed4f04eb871d7f01d
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-12 16:07:11 -0400

    Desafio 055 - Maior e menor da sequência

---
+++ Exercicio_055/desafio_055.py
@@ -0,0 +1,21 @@
+#   Desafio 055 - Referente aula Fase13
+#   Faça um programa que leia o peso de cinco pessoas.
+#   No final, mostre qual foi o maior e o menor peso lidos.
+
+maior = 0
+menor = 0
+
+for pessoa in range(1, 6):
+    peso = float(input('Peso da {}ª pessoa : '.format(pessoa)))
+
+    if pessoa == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        elif peso < menor:
+            menor = peso
+
+print('O maior peso lido foi de {}Kg.'.format(maior))
+print('O menor peso lido foi de {}Kg.'.format(menor))
