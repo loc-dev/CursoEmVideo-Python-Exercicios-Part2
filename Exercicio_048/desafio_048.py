f9f7fbde172c03c234109adfb6d60737d4b52dff
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-12 10:57:12 -0400

    Desafio 048 - Soma ímpares múltiplos de três

---
+++ Exercicio_048/desafio_048.py
@@ -0,0 +1,13 @@
+#   Desafio 048 - Referente aula Fase13
+#   Faça um programa que calcule a soma entre todos os números ímpares
+#   que são múltiplos de três e que se encontram no intervalo de 1 até 500.
+
+soma = 0
+cont = 0
+
+for i in range(1, 501, 2):
+    if i % 3 == 0:
+        cont += 1
+        soma += i
+
+print('A soma de todos os {} valores solicitados é {}'.format(cont, soma))
