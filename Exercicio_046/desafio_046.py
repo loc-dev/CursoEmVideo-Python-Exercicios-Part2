edb6025c6bdffecdf05df84c510a295215cb982f
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-12 10:19:33 -0400

    Desafio 046 - Contagem regressiva

---
+++ Exercicio_046/desafio_046.py
@@ -0,0 +1,10 @@
+#   Desafio 046 - Referente aula Fase13
+#   Faça um programa que mostre na tela uma contagem regressiva
+#   para o estouro de um fogos de artifício, indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
+from time import sleep
+
+for cont in range(10, -1, -1):
+    print(cont)
+    sleep(1)
+print('BUM! BUM! POOOW!')
