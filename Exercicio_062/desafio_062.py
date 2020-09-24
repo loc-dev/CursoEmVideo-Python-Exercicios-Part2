f06fab09d92b5f2ebc2c61f63bd6709d335b6b9c
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-24 18:09:37 -0400

    Desafio 062 - Super Progressão Aritmética v3.0

---
+++ Exercicio_062/desafio_062.py
@@ -0,0 +1,25 @@
+#   Desafio 062 - Referente aula Fase14
+#   Melhore o Desafio 061, perguntando para o usuário se ele quer
+#   mostrar mais alguns termos. O programa encerra quando ele disser que quer
+#   mostrar 0 termos.
+
+print('Gerador de PA')
+print('-=' * 10)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print('{} -> '.format(termo), end='')
+        termo += razao
+        cont += 1
+    print('PAUSA')
+    mais = int(input('Quantos termos você quer mostrar a mais? '))
+print('Progressão finalizada com {} termos mostrados.'.format(total))
+print('FIM')
