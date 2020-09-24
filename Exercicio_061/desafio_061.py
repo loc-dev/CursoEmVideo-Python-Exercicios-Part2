e6cb20073ffbfa9350689c0035ec532ae1479cf6
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-09-24 17:52:47 -0400

    Desafio 061 - Progressão Aritmética v2.0

---
+++ Exercicio_061/desafio_061.py
@@ -0,0 +1,17 @@
+#   Desafio 061 - Referente aula Fase14
+#   Refaça o Desafio 051, lendo o primeiro termo e a razão de uma PA,
+#   mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
+print('Gerador de PA')
+print('-=' * 10)
+
+primeiro = int(input('Primeiro termo: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+
+while cont <= 10:
+    print('{} -> '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')
