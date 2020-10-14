677594cbb3792fbc3d595b0f5e4e4e0579b1a545
Leonardo Oliveira <leonardoc.developer@gmail.com>
2020-10-14 10:19:45 -0400

    Desafio 067 - Tabuada v3.0

---
+++ Exercicio_067/desafio_067.py
@@ -0,0 +1,19 @@
+#   Desafio 067 - Referente aula 15
+#   Faça um programa que mostre a tabuada de vários números,
+#   um de cada vez, para cada valor digitado pelo usuário.
+#   O programa será interrompido quando o número solicitado for negativo.
+
+while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    
+    print('-=-' * 15)
+
+    if n < 0:
+        break
+    
+    for c in range(1, 11):
+        print(f'{n} x {c} = {n * c}')
+
+    print('-=-' * 15)
+
+print('PROGRAMA TABUADA ENCERRADO. Volte sempre !')
